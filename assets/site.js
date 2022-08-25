ea76b47874b302ee010a105818d6e463473c0b8b
Jeff Rynes <jrynes3@gmail.com>
2022-08-25 11:27:01 +0200

    Formatting and commenting
    Moderate push for formatting and commenting, and general cleanup

--- assets/site.js
+++ assets/site.js
@@ -1,44 +1,4 @@
 //Javascript code here
 
-$('#myCarousel').carousel({
-    interval: 4000
-  })
-  
-$('.carousel .item').each(function(){
-var next = $(this).next();
-if (!next.length) {
-    next = $(this).siblings(':first');
-}
-next.children(':first-child').clone().appendTo($(this));
-
-for (var i=0;i<2;i++) {
-    next=next.next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-
-    next.children(':first-child').clone().appendTo($(this));
-}
-});
-
-function carouselNext(){
-    console.log("test")
-    $('.carousel .item').each(function(){
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-        
-        for (var i=0;i<2;i++) {
-            next=next.next();
-            if (!next.length) {
-            next = $(this).siblings(':first');
-            }
-        
-            next.children(':first-child').clone().appendTo($(this));
-        }
-        });
-}
 
 
