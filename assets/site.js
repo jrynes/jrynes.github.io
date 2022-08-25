066aeee28f8ac271d2528ec67ecee33c2f1c83e1
Jeff Rynes <jrynes3@gmail.com>
2022-08-25 09:55:02 +0200

    Homepage and Weekend Update edits
    Major homepage updates and minor weekend update edits

--- assets/site.js
+++ assets/site.js
@@ -1,42 +1,44 @@
 //Javascript code here
 
-// Add script to replace SVG graphic links with inline SVG code, for easier edits
-/**
-* Replace all SVG images with inline SVG
-*/
-
-function replaceSVG() {
-    /**
-     * Replace all SVG images with inline SVG
-     */
-     jQuery('img.svg').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
-
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
-        console.log("Success")
-
-    });
+$('#myCarousel').carousel({
+    interval: 4000
+  })
+  
+$('.carousel .item').each(function(){
+var next = $(this).next();
+if (!next.length) {
+    next = $(this).siblings(':first');
 }
+next.children(':first-child').clone().appendTo($(this));
+
+for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+}
+});
+
+function carouselNext(){
+    console.log("test")
+    $('.carousel .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<2;i++) {
+            next=next.next();
+            if (!next.length) {
+            next = $(this).siblings(':first');
+            }
+        
+            next.children(':first-child').clone().appendTo($(this));
+        }
+        });
+}
+
+
